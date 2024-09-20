171a6c4bcfe670f89f19b993ce0bfd37bce73d0a
sina <anonymous8884person@gmail.com>
2024-09-20 13:54:16 +0330

    main updated

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
       <div className='position-relative'>
         <img src="../Main-img/oip 1.jpg" alt=""/>
         <div className='img-information'>
-          <h2>من یک یرنامه نویس</h2>
+          <h2>من یک برنامه نویس</h2>
           <p>مسلط به چندین زبان برنامه نویسی، کلیه امور گرافیکی و طراحی سایت با چندین سال تجربه مفید.</p>
           <Buttons text="تماس با من"/>
         </div>
